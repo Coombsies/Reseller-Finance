cd375044bd714b78aabe47ef3463798e9d550677
Coombsies <coombsiescreations@outlook.com>
2026-06-08 07:29:33 -0400

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js'
-
-const supabaseUrl = 'https://dzvgbytxbifhnxckmiih.supabase.co'
-const supabaseAnonKey = 'sb_publishable_pvsbykWKiWvRUZsWiabvIQ_pMIRKZ2R'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
