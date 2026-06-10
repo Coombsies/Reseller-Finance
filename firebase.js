60fcadb4b6913e87f76fbe3b02f725218cffdf14
Coombsies <coombsiescreations@outlook.com>
2026-06-10 11:22:12 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBdjlO1S_BMExJt25laptFFCSlIkzOcxqI",
   authDomain: "resellerfinanceapp.firebaseapp.com",
@@ -6,3 +10,9 @@ const firebaseConfig = {
   messagingSenderId: "625217355771",
   appId: "1:625217355771:web:3c50262a61894622c1555e"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+export const db = getFirestore(app);
